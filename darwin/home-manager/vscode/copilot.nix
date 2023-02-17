888e773868e6723ec5bfd7da4f309976710d266c
frekw <fredrik@warnsberg.se>
2023-02-17 12:58:06 +0100

    bump deps

--- darwin/home-manager/vscode/copilot.nix
+++ darwin/home-manager/vscode/copilot.nix
@@ -6,7 +6,7 @@ pkgs.vscode-utils.buildVscodeMarketplaceExtension {
     publisher = "github";
     name = "copilot";
     version = "1.73.8685";
-    sha256 = "sha256-MFFh3WRWWoFvIqpvLoB/E5ozyK/N7OysoMfaDNO7OqQ=";
+    sha256 = "sha256-W1j1VAuSM1sgxHRIahqVncUlknT+MPi7uutY+0NURZQ=";
   };
   meta = { license = lib.licenses.unfree; };
 }
