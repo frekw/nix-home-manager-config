612f0b4e4194e90eeac8251fc39cda5cd6da0eda
frekw <fredrik@warnsberg.se>
2024-03-19 08:44:37 +0100

    fix vscode theme

--- home/base/vscode/default.nix
+++ home/base/vscode/default.nix
@@ -1,103 +1,105 @@
-{pkgs, ...}: {
+{ pkgs, ... }: {
   programs.vscode = {
     enable = true;
     mutableExtensionsDir = false;
-    extensions = with pkgs.vscode-extensions; [
-      brettm12345.nixfmt-vscode
-      github.copilot
-      elixir-lsp.vscode-elixir-ls
-      graphql.vscode-graphql
-      golang.go
-      hashicorp.terraform
-      jnoortheen.nix-ide
-      matklad.rust-analyzer
-      ocamllabs.ocaml-platform
-      scala-lang.scala
-      scalameta.metals
-      vscodevim.vim
-      vspacecode.vspacecode
-      vspacecode.whichkey
-      zxh404.vscode-proto3
-    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
-      {
-        name = "vscode-elixir";
-        publisher = "mjmcloug";
-        version = "1.1.0";
-        sha256 = "sha256-EE4x75ljGu212gqu1cADs8bsXLaToVaDnXHOqyDlR04=";
-      }
-      {
-        name = "monochrome-dark";
-        publisher = "mikehhodgson";
-        version = "0.0.7";
-        sha256 = "sha256-W+QNykFE5wE/huW1f+JA0+OcIBmtrbSLD/y54REqhEE=";
-      }
-      {
-        name = "monochrome";
-        publisher = "anotherglitchinthematrix";
-        version = "2.4.3";
-        sha256 = "c64e184d3e9ad515bf2675a5fabeced1cdd937f95933bba212709cf61eec9997";
-      }
-      {
-        publisher = "mogelbrod";
-        name = "quickopener";
-        version = "0.4.1";
-        sha256 = "sha256-AJ3C1QdGpsobSMyu7xes+rC4EW/NravZWS60GqEKW2Y=";
-      }
-      {
-        name = "apc-extension";
-        publisher = "drcika";
-        version = "0.3.0";
-        sha256 = "sha256-do3QYBq83XcqD2jSMC+q+2mQHPiodpDA+OJdT0Zh7uc=";
-      }
-      {
-        name = "vscode-bazel";
-        publisher = "BazelBuild";
-        version = "0.7.0";
-        sha256 = "sha256-/a34MMsHy7zmGrVAtjMWKmulwS+lip3J1YugkACMmxc=";
-      }
-      {
-        name = "graphql";
-        publisher = "mquandalle";
-        version = "0.1.2";
-        sha256 = "sha256-0hBtHDD/yk+EpA0A/9ovAz8SUcAoTrXAtHU4Y9MiHnw=";
-      }
-      {
-        name = "direnv";
-        publisher = "mkhl";
-        version = "0.15.2";
-        sha256 = "sha256-agJfEc4JqxKaNQhto0BfVx2Pxk809G0Ne3TqaR8mlxo=";
-      }
-      {
-        name = "authzed";
-        publisher = "chiperific";
-        version = "1.1.0";
-        sha256 = "sha256-GmZowlnf+0JjN5ilR5aWZyR06pQKzuydhCeEWXnRvfs=";
-      }
-      {
-        name = "gti-vscode";
-        publisher = "Graphite";
-        version = "0.5.4";
-        sha256 = "sha256-mD6bLyToVcckADdUbj1s973As+DSnPsJLMjDv3omO6I=";
-      }
-      {
-        name = "toml";
-        publisher = "be5invis";
-        version = "0.6.0";
-        sha256 = "sha256-yk7buEyQIw6aiUizAm+sgalWxUibIuP9crhyBaOjC2E=";
-      }
-      {
-        name = "python";
-        publisher = "ms-python";
-        version = "2024.3.10601007";
-        sha256 = "sha256-7BmzlsgsCOUvEVhiIL6v7ItvVOqqfTsdEMZYieurmyc=";
-      }
-      # {
-      #   publisher = "github";
-      #   name = "copilot";
-      #   version = "1.158.0";
-      #   sha256 = "sha256-W1j1VAuSM1sgxHRIahqVncUlknT+MPi7uutY+0NURZQ=";
-      # }
-    ];
+    extensions = with pkgs.vscode-extensions;
+      [
+        brettm12345.nixfmt-vscode
+        github.copilot
+        elixir-lsp.vscode-elixir-ls
+        graphql.vscode-graphql
+        golang.go
+        hashicorp.terraform
+        jnoortheen.nix-ide
+        matklad.rust-analyzer
+        ocamllabs.ocaml-platform
+        scala-lang.scala
+        scalameta.metals
+        vscodevim.vim
+        vspacecode.vspacecode
+        vspacecode.whichkey
+        zxh404.vscode-proto3
+      ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+        {
+          name = "vscode-elixir";
+          publisher = "mjmcloug";
+          version = "1.1.0";
+          sha256 = "sha256-EE4x75ljGu212gqu1cADs8bsXLaToVaDnXHOqyDlR04=";
+        }
+        {
+          name = "monochrome-dark";
+          publisher = "mikehhodgson";
+          version = "0.0.7";
+          sha256 = "sha256-W+QNykFE5wE/huW1f+JA0+OcIBmtrbSLD/y54REqhEE=";
+        }
+        # {
+        #   name = "monochrome";
+        #   publisher = "anotherglitchinthematrix";
+        #   version = "2.4.3";
+        #   sha256 =
+        #     "c64e184d3e9ad515bf2675a5fabeced1cdd937f95933bba212709cf61eec9997";
+        # }
+        {
+          publisher = "mogelbrod";
+          name = "quickopener";
+          version = "0.4.1";
+          sha256 = "sha256-AJ3C1QdGpsobSMyu7xes+rC4EW/NravZWS60GqEKW2Y=";
+        }
+        {
+          name = "apc-extension";
+          publisher = "drcika";
+          version = "0.3.0";
+          sha256 = "sha256-do3QYBq83XcqD2jSMC+q+2mQHPiodpDA+OJdT0Zh7uc=";
+        }
+        {
+          name = "vscode-bazel";
+          publisher = "BazelBuild";
+          version = "0.7.0";
+          sha256 = "sha256-/a34MMsHy7zmGrVAtjMWKmulwS+lip3J1YugkACMmxc=";
+        }
+        {
+          name = "graphql";
+          publisher = "mquandalle";
+          version = "0.1.2";
+          sha256 = "sha256-0hBtHDD/yk+EpA0A/9ovAz8SUcAoTrXAtHU4Y9MiHnw=";
+        }
+        {
+          name = "direnv";
+          publisher = "mkhl";
+          version = "0.15.2";
+          sha256 = "sha256-agJfEc4JqxKaNQhto0BfVx2Pxk809G0Ne3TqaR8mlxo=";
+        }
+        {
+          name = "authzed";
+          publisher = "chiperific";
+          version = "1.1.0";
+          sha256 = "sha256-GmZowlnf+0JjN5ilR5aWZyR06pQKzuydhCeEWXnRvfs=";
+        }
+        {
+          name = "gti-vscode";
+          publisher = "Graphite";
+          version = "0.5.4";
+          sha256 = "sha256-mD6bLyToVcckADdUbj1s973As+DSnPsJLMjDv3omO6I=";
+        }
+        {
+          name = "toml";
+          publisher = "be5invis";
+          version = "0.6.0";
+          sha256 = "sha256-yk7buEyQIw6aiUizAm+sgalWxUibIuP9crhyBaOjC2E=";
+        }
+        {
+          name = "python";
+          publisher = "ms-python";
+          version = "2024.3.10601007";
+          sha256 = "sha256-7BmzlsgsCOUvEVhiIL6v7ItvVOqqfTsdEMZYieurmyc=";
+        }
+        # {
+        #   publisher = "github";
+        #   name = "copilot";
+        #   version = "1.158.0";
+        #   sha256 = "sha256-W1j1VAuSM1sgxHRIahqVncUlknT+MPi7uutY+0NURZQ=";
+        # }
+      ];
 
     userSettings = {
       "[nix]"."editor.tabSize" = 2;
@@ -107,17 +109,11 @@
       "vim.useSystemClipboard" = true;
       "vim.normalModeKeyBindingsNonRecursive" = [
         {
-          "before" = [
-            "<space>"
-          ];
-          "commands" = [
-            "vspacecode.space"
-          ];
+          "before" = [ "<space>" ];
+          "commands" = [ "vspacecode.space" ];
         }
         {
-          "before" = [
-            ","
-          ];
+          "before" = [ "," ];
           "commands" = [
             "vspacecode.space"
             {
@@ -129,17 +125,11 @@
       ];
       "vim.visualModeKeyBindingsNonRecursive" = [
         {
-          "before" = [
-            "<space>"
-          ];
-          "commands" = [
-            "vspacecode.space"
-          ];
+          "before" = [ "<space>" ];
+          "commands" = [ "vspacecode.space" ];
         }
         {
-          "before" = [
-            ","
-          ];
+          "before" = [ "," ];
           "commands" = [
             "vspacecode.space"
             {
@@ -166,7 +156,7 @@
       "editor.minimap.enabled" = false;
       "editor.inlayHints.enabled" = "on";
       "editor.inlayHints.fontSize" = 11;
-      "editor.tokenColorCustomizations" = {};
+      "editor.tokenColorCustomizations" = { };
 
       "metals.suggestLatestUpgrade" = true;
       "metals.serverVersion" = "1.2.0+106-c6ab0475-SNAPSHOT";
