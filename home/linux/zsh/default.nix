0168224fc3476b2fe36e40702f3b4b1a899f700b
frekw <fredrik@warnsberg.se>
2024-04-12 00:02:34 +0200

    add build

--- home/linux/zsh/default.nix
+++ home/linux/zsh/default.nix
@@ -1,6 +1,8 @@
 { ... }: {
   programs.zsh = {
     shellAliases = {
+      build =
+        "cd ~/src/priv/nix-home-manager-config && nix flake check && cd -";
       switch =
         "sudo nixos-rebuild switch --flake ~/src/priv/nix-home-manager-config";
     };
