58012d99083967d7d7c614e433b9b6a1579af7c5
frekw <fredrik@warnsberg.se>
2024-08-12 09:52:48 +0200

    ...

--- pkgs/darwin/firefox/default.nix
+++ pkgs/darwin/firefox/default.nix
@@ -8,7 +8,7 @@
 
 stdenv.mkDerivation rec {
   pname = "firefox-darwin";
-  version = "127.0";
+  version = "129.0";
 
   buildInputs = [ undmg ];
   sourceRoot = ".";
@@ -24,7 +24,7 @@ stdenv.mkDerivation rec {
   src = fetchurl {
     name = "Firefox-${version}.dmg";
     url = "https://download-installer.cdn.mozilla.net/pub/firefox/releases/${version}/mac/en-US/Firefox%20${version}.dmg";
-    sha256 = "sha256-PDDLyK4q0NxErDKiZshxvQhmyiwodrRkyGGTrl/+bm0=";
+    sha256 = "sha256-S4bp3ismM5swNCoe874eORTXCSH4H9Z7ycY1lYIwUfE=";
   };
 
   meta = with lib; {
