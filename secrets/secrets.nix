18dd91945095185a261be9c337274d742627afcf
frekw <fredrik@warnsberg.se>
2025-04-09 22:49:51 +0200

    workwork

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -34,5 +34,6 @@ in
 
   "cf-tf/access-key-id.age".publicKeys = allHosts;
   "cf-tf/secret.age".publicKeys = allHosts;
+  "cf-tf/bucket.age".publicKeys = allHosts;
   "cf-tf/endpoint.age".publicKeys = allHosts;
 }
