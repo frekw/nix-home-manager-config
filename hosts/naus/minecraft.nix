6018c37aed9168b1388ba4a07f0133c2a97d8aea
frekw <fredrik@warnsberg.se>
2025-04-17 21:16:21 +0200

    workwork

--- hosts/naus/minecraft.nix
+++ hosts/naus/minecraft.nix
@@ -53,7 +53,7 @@
 
         MEMORY = "";
         ENABLE_WHITELIST = "true";
-        WHITELIST = "40e195fe-6fe9-4c31-a263-b446851bbe12,00000000-0000-0000-0009-01f9df2f1ded,00000000-0000-0000-0009-01facc8a9af9,81fcd61d-5091-450d-b7ee-7fc81bba5ca8";
+        WHITELIST = "40e195fe-6fe9-4c31-a263-b446851bbe12,00000000-0000-0000-0009-01f9df2f1ded,00000000-0000-0000-0009-01facc8a9af9,81fcd61d-5091-450d-b7ee-7fc81bba5ca8,00000000-0000-0000-01F3-BA3C24DA";
         SEED = "44647503765050661";
         PLUGINS = ''
           https://download.geysermc.org/v2/projects/geyser/versions/latest/builds/latest/downloads/spigot
