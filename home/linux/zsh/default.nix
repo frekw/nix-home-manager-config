7644c9cdda2046adbe164ad3bdc65ea776c5f25a
frekw <fredrik@warnsberg.se>
2025-06-23 09:17:42 +0200

    workwork

--- home/linux/zsh/default.nix
+++ home/linux/zsh/default.nix
@@ -3,7 +3,7 @@
   programs.zsh = {
     shellAliases = {
       build = "cd ~/src/priv/nix-home-manager-config && nix flake check && cd -";
-      switch = "nixos-rebuild switch --flake ~/src/priv/nix-home-manager-config --use-remote-sudo";
+      switch = "nixos-rebuild switch --flake ~/src/priv/nix-home-manager-config --sudo";
     };
   };
 }
