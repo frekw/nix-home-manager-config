da0dfa8f3eb07a6aaa514478039cd852da64290a
frekw <fredrik@warnsberg.se>
2025-11-24 09:45:07 +0100

    workwork

--- home/base/ssh.nix
+++ home/base/ssh.nix
@@ -1,23 +1,46 @@
-{ ... }:
+{ lib, ... }:
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
-    matchBlocks = {
-      "*" = {
-        forwardAgent = false;
-        addKeysToAgent = "no";
-        compression = false;
-        serverAliveInterval = 0;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        userKnownHostsFile = "~/.ssh/known_hosts";
-        controlMaster = "no";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        controlPersist = "no";
-      };
-    };
-    extraConfig = ''
+    # matchBlocks = {
+    #   "*" = {
+    #     forwardAgent = false;
+    #     addKeysToAgent = "no";
+    #     compression = false;
+    #     serverAliveInterval = 0;
+    #     serverAliveCountMax = 3;
+    #     hashKnownHosts = false;
+    #     userKnownHostsFile = "~/.ssh/known_hosts";
+    #     controlMaster = "no";
+    #     controlPath = "~/.ssh/master-%r@%n:%p";
+    #     controlPersist = "no";
+    #   };
+    # };
+    # extraConfig = ''
+    #   IgnoreUnknown UseKeychain
+    #   AddKeysToAgent yes
+    #   UseKeychain yes
+    #   IdentityFile ~/.ssh/id_ed25519
+    # '';
+  };
+
+  # work around to get 600 permissions on .ssh/config
+  home.file.".ssh/config" = {
+    text = ''
+      Host *
+        ForwardAgent no
+        AddKeysToAgent no
+        Compression no
+        ServerAliveInterval 0
+        ServerAliveCountMax 3
+        HashKnownHosts no
+        UserKnownHostsFile ~/.ssh/known_hosts
+        ControlMaster no
+        ControlPath ~/.ssh/master-%r@%n:%p
+        ControlPersist no
+
+      # Extra custom lines you used to put in extraConfig
       IgnoreUnknown UseKeychain
       AddKeysToAgent yes
       UseKeychain yes
